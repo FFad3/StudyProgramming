4bb7c973f16061b28ec44e8a2cd14bf78b10528a
FFad3 <patgne@wp.pl>
2021-10-17 00:32:58 +0200

    SimpleStack using generics

--- GenericsPractice/Program.cs
+++ GenericsPractice/Program.cs
@@ -6,7 +6,33 @@ namespace GenericsPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StackDoubles();
+            StackStrings();
+            Console.ReadLine();
+        }
+
+        private static void StackStrings()
+        {
+            var stack = new SimpleStack<string>();
+            stack.Push("Wierd Brain Coffe");
+            stack.Push("Exmaple text");
+        }
+
+        private static void StackDoubles()
+        {
+            var stack = new SimpleStack<double>();
+            stack.Push(1.2);
+            stack.Push(2.8);
+            stack.Push(3.0);
+
+            double sum = 0.0;
+            while (stack.Count() > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+            }
+            Console.WriteLine(sum);
         }
     }
 }
