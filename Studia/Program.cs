7739f3f1dcf49957a7e924004188ccbb089f1736
FFad3 <patgne@wp.pl>
2021-10-31 19:41:31 +0100

    YieldReturn

--- Studia/Program.cs
+++ Studia/Program.cs
@@ -9,7 +9,8 @@ namespace Studia
         {
             ProcessPeople();
         }
-
+        /* First version without - Yelid return
+         
         public static void ProcessPeople()
         {
             var people = GetPeaople(1_000_000);
@@ -31,5 +32,32 @@ namespace Studia
             }
             return people;
         }
+        */
+
+        public static void ProcessPeople()
+        {
+            var people = GetPeaople(1_000_000);
+            Console.WriteLine(people.GetEnumerator());       
+            foreach (var person in people)
+            {
+                if (person.Id < 1000)
+                    Console.WriteLine($"Id: {person.Id}, Name: {person.Name}");
+                else
+                    break;
+            }
+        }
+
+        public static IEnumerable<Person> GetPeaople(int count)
+        {
+            List<Person> people = new();
+            for (int i = 0; i < count; i++)
+            {
+               yield return new Person() { Id = i, Name = $"Name {i}" };
+            }
+            /* lazy iteration allows to get one object at a time and remember the state of the loop 
+             * usefull in huge amount of data less memory allocated
+             */
+        }
+
     }
 }
